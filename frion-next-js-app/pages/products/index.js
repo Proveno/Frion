ad4d55a23c4badb6a2c867345a0e6fa8a4e21d19
MaksymReznik <reznikmaksym14@gmail.com>
2021-12-29 18:54:27 +0200

    new search line position

--- frion-next-js-app/pages/products/index.js
+++ frion-next-js-app/pages/products/index.js
@@ -55,32 +55,34 @@ const Index = ({ products }) => {
                 {/* NavButtons */}
                 {/* TODO: replace links to scroll */}
                 <div className={`flex justify-between w-full`}>
-                    <div className={`mx-12 w-full self-center flex justify-start`}>
-                        <div className="flex w-1/2 pl-16">
-                        <input className="w-full rounded-lg px-2 mr-2"  placeholder="Enter title...">
-                        </input>
-                        <button className={`${shopBlock.searchButton} font-medium px-8 ml-2 py-1 rounded-lg`}>Search</button>
+                <div className={`mr-16 w-full self-center flex justify-end`}>
+                        <div className="flex w-1/2 pl-24">
+                            <input className="w-full rounded px-2 mr-2" placeholder="Enter title...">
+                            </input>
+                            <button className={`${shopBlock.searchButton} font-medium px-8 ml-2 py-1 rounded-lg`}>Search</button>
                         </div>
                     </div>
-                    <div className="self-center mx-2">
-                        <select
-                            className={`${navBar.langButton} px-4 text`}
-                            id="LanguageSelect"
-                            onChange={() => {
-                                router.push(
-                                    `/${document.getElementById("LanguageSelect").value}`
-                                );
-                            }}
-                            defaultValue={router.locale}
-                        >
-                            <option value="en">{t.english}</option>
-                            <option value="ru">{t.russian}</option>
-                            <option value="de">{t.deutsch}</option>
-                            <option value="uk">{t.ukrainian}</option>
-                        </select>
-                    </div>
-                    <div className="self-center flex">
-                        <Image width="39px" height="39px" src={cartIcon}></Image>
+                    <div className="flex justify-center w-1/6">
+                        <div className="self-center mx-4">
+                            <select
+                                className={`${navBar.langButton} px-4 text`}
+                                id="LanguageSelect"
+                                onChange={() => {
+                                    router.push(
+                                        `/${document.getElementById("LanguageSelect").value}`
+                                    );
+                                }}
+                                defaultValue={router.locale}
+                            >
+                                <option value="en">{t.english}</option>
+                                <option value="ru">{t.russian}</option>
+                                <option value="de">{t.deutsch}</option>
+                                <option value="uk">{t.ukrainian}</option>
+                            </select>
+                        </div>
+                        <div className="self-center flex mx-4" >
+                            <Image width="39px" height="39px" src={cartIcon}></Image>
+                        </div>
                     </div>
                 </div>
             </div>
