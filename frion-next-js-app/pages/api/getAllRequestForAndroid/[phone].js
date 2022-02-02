db6b4f371355cba5130c3913c3e79c3c5a9542d1
Maksym <reznikmaksym14@gmail.com>
2022-02-03 00:31:42 +0200

    add api for all requests

---
+++ frion-next-js-app/pages/api/getAllRequestForAndroid/[phone].js
@@ -0,0 +1,55 @@
+import dbConnect from "../../../utils/dbConnect";
+import Taking from "../../../models/Taking";
+import Giving from "../../../models/Giving";
+import Healing from "../../../models/Healing";
+
+
+dbConnect();
+
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+export default async (req, res) => {
+  const {
+    query: { phone },
+    method,
+  } = req;
+  switch (method) {
+    case "GET":
+      try {
+        const regex = new RegExp(escapeRegex(phone), "gi");
+
+        const taking = await Taking.find({
+            phone: phone,
+        });
+        const giving = await Giving.find({
+            phone: phone,
+        });
+        const healing = await Healing.find({
+            phone: phone,
+        });
+
+
+
+        
+        const allGet = [
+          ...taking,
+          ...giving,
+          ...healing
+        ];
+        
+        if (!allGet.length > 0) {
+          return res.status(404).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, allRequestData: allGet });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
